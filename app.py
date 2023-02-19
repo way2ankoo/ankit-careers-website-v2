890f376e5866d7d1979d700253cc02df3bad664c
ankit rawat <80580954+way2ankoo@users.noreply.github.com>
2023-02-19 21:29:05 +0000

    dynamic pages added for jobs

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db
+from database import load_jobs_from_db, load_job_from_db
 
 app = Flask(__name__)
 
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
   jobs = load_jobs_from_db()
-  return render_template('home.html', jobs=jobs, company_name="Ankit_R")
+  return render_template('home.html', jobs=jobs)
 
 
 @app.route("/api/jobs")
@@ -15,6 +15,12 @@ def list_jobs():
   jobs = load_jobs_from_db()
   return jsonify(jobs)
 
+@app.route("/job/<id>")
+def show_job(id):
+  job = load_job_from_db(id)
+  if not job:
+    return "Not Found"
+  return render_template('jobpage.html', job=job)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
--- database.py
+++ database.py
@@ -3,19 +3,27 @@ import os
 
 db_connection_string = os.environ['DB_CONNECTION_STRING']
 
-engine = create_engine(
-  db_connection_string,
-  connect_args={
-    "ssl" : {
-      "ssl_ca": "/etc/ssl/cert.pem"
-    }
-  }
-)
+engine = create_engine(db_connection_string,
+                       connect_args={"ssl": {
+                         "ssl_ca": "/etc/ssl/cert.pem"
+                       }})
+
 
 def load_jobs_from_db():
   with engine.connect() as conn:
     result = conn.execute(text("select * from jobs"))
     jobs = []
     for row in result.all():
-      jobs.append(row._mapping)
+      jobs.append(dict(row._mapping))
     return jobs
+
+
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(text('SELECT * from jobs WHERE id = :val'),
+                          {'val': id})
+    rows = result.all()
+    if len(rows) == 0:
+      return None
+    else:
+      return dict(rows[0]._mapping)
